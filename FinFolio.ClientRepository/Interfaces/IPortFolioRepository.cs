6c761577d1ad18003a4697eba3477f1d538198eb
vishnudassoman <vishnu.vichu@gmail.com>
2022-07-18 22:39:48 +0530

    Added wishlist and portfolio interfaces

--- FinFolio.ClientRepository/Interfaces/IPortFolioRepository.cs
+++ FinFolio.ClientRepository/Interfaces/IPortFolioRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace FinFolio.PortFolioRepository.Interfaces
+﻿using FinFolio.PortFolioRepository.Entities;
+
+namespace FinFolio.PortFolioRepository.Interfaces
 {
     public interface IPortFolioRepository
     {
+        public PortFolio GetPortFolioById(int id);
+        public PortFolio GetPortFolioByUserId(int userId);
+        public PortFolio CreatePortFolio(PortFolio portFolio);
+        public PortFolio UpdatePortFolio(PortFolio portFolio);
     }
 }
---
+++ FinFolio.ClientRepository/Interfaces/IWishListRepository.cs
@@ -0,0 +1,12 @@
+﻿using FinFolio.PortFolioRepository.Entities;
+
+namespace FinFolio.PortFolioRepository.Interfaces
+{
+    public interface IWishlistRepository
+    {
+        public Wishlist GetWishlistByUserId(int userId);
+        public Wishlist GetWishlistById(int id);
+        public Wishlist CreateWishList(Wishlist wishlist);
+        public Wishlist UpdateWishlist(Wishlist wishlist);
+    }
+}
