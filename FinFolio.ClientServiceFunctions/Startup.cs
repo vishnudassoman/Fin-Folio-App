80b932ea40a97ff185ef138c1f95ccccd100685a
vishnudassoman <vishnu.vichu@gmail.com>
2022-07-26 18:01:08 +0530

    Added startup class for functions to configure dependency injection.

---
+++ FinFolio.ClientServiceFunctions/Startup.cs
@@ -0,0 +1,38 @@
+﻿using FinFolio.PortFolioCore.Interfaces;
+using FinFolio.PortFolioCore.Services;
+using FinFolio.PortFolioRepository;
+using FinFolio.PortFolioRepository.Interfaces;
+using FinFolio.PortFolioRepository.Repository;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FinFolio.PortFolio.WebAPI.Startup))]
+namespace FinFolio.PortFolio.WebAPI
+{
+    public class Startup : FunctionsStartup
+    {
+        private IConfiguration _configuration;
+
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            _configuration = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+            builder.Services.AddDbContext<PortFolioDBContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString("PortFolioDBConnection")
+            ));
+            //Repository Dependency
+            builder.Services.AddScoped<IWishlistRepository, WishlistRepository>();
+            //Core Service Dependency
+            builder.Services.AddScoped<IWishlistService, WishlistService>();
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+        }
+    }
+}
+
